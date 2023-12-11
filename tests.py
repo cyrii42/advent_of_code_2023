1bad3f5d7400fcadf3a1ef86ac2d62f860a8a44b
Zach Vaughan <85461794+cyrii42@users.noreply.github.com>
2023-12-10 23:25:56 -0500

    updates

--- tests.py
+++ tests.py
@@ -4,6 +4,9 @@
 
 # print(len(symbol_list) - 1)
 
-asdf = "a----b"
-print(asdf[0])
-print(asdf[-1])
+# asdf = "a----b"
+# print(asdf[0])
+# print(asdf[-1])
+
+num_list = [54, 53, 99, 100, 561, 999, 109234, 4]
+print([x for x in num_list if x in range(54, 100)])
